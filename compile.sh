62bf256d5d33c7b1f3648c15b30a7384c7d0127a
Kyle Rodgers <kylerodgers@Kyles-MacBook-Pro.local>
2014-06-17 11:40:30 -0500

    Added compile script

---
+++ compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+g++ -c Engine.cc
+g++ -c Node.cc
+g++ -c PetersenGraph.cc
+g++ -c CompleteGraph.cc
+g++ -c PartitionedGraph.cc
+g++ -c Token.cc
+g++ -c RandomEngine.cc
+g++ *.o main.cc -o main
+mkdir -p output_data
+./main
